15fb3d26898f1e481a836c0c28007901e5c3419a
Jhoskx <jhonatan.ja293@gmail.com>
2023-12-15 22:29:08 -0500

    Creacion del controller

---
+++ Tesis-DDD.Api/Controllers/Screens1Controllers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tesis_DDD.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Screens1Controllers : ControllerBase
+    {
+    }
+}
