ac74e201dd93f07c8dd596166cf034a25777837e
JhonatanAmaya <116816323+JhonatanAmaya@users.noreply.github.com>
2024-01-02 11:18:41 -0500

    as

---
+++ Api-DDD.Domain/Resources.cs
@@ -0,0 +1,11 @@
+﻿using Api_DDD.Domain.Common;
+
+namespace Api_DDD.Domain
+{
+    public class Resources:MasterEntity
+    {
+        public string Name {get; set;}
+        public int ProjectId { get; set; }
+        public int ExperienceId { get; set; }
+    }
+}
