5f2dea9122b1e32e5b4cb64fd540e477af26a890
JhonatanAmaya <116816323+JhonatanAmaya@users.noreply.github.com>
2024-01-02 11:53:34 -0500

    create File BasesSpesification

---
+++ Tesis-DDD.Application/Specifications/BaseSpecification.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using Tesis_DDD.Application.Contracts.Persistence;
+
+namespace Tesis_DDD.Application.Specifications
+{
+    public class BaseSpecification<T>:ISpecification<T>
+    {
+        public BaseSpecification()
+        {
+        }
+        public BaseSpecification(Expression<Func<T, bool>> criteria) => Criteria = criteria;
+
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public List<string> IncludeStrings { get; } = new List<string>();
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression) => OrderBy = orderByExpression;
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression) => OrderByDescending = orderByDescendingExpression;
+
+        public int Take { get; private set; }
+        public int Skip { get; private set; }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+
+        public bool IsPagingEnabled { get; private set; }
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression) => Includes.Add(includeExpression);
+        protected void AddInclude(string includeString) => IncludeStrings.Add(includeString);
+    }
+}
